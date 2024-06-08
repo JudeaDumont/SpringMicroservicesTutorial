384393e8271577e4f6584952ac80f3aa3db9ce72
dumont, judea <dumontjudea2@gmail.com>
2024-06-08 11:43:07 -0700

    Adding the orderservice, though it throws 500s if you place an order, probably because you have to construct the sql table with a list of objects

---
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -0,0 +1,36 @@
+package com.example.order_service.service;
+
+import com.example.order_service.dto.OrderLineItemsDto;
+import com.example.order_service.dto.OrderRequest;
+import com.example.order_service.model.Order;
+import com.example.order_service.model.OrderLineItems;
+import com.example.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setOrderLineItemsList(orderRequest
+                .getOrderLineItemsDtoList()
+                .stream().map(this::mapToDto).toList());
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        return OrderLineItems.builder()
+                .id(orderLineItemsDto.getId())
+                .price(orderLineItemsDto.getPrice())
+                .quantity(orderLineItemsDto.getQuantity())
+                .skuCode(orderLineItemsDto.getSkuCode())
+                .build();
+    }
+}
