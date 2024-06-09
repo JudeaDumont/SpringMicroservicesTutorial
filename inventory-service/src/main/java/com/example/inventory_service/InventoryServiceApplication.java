7ad4a634c81740ce498eb89a03022185e6db58d4
dumont, judea <dumontjudea2@gmail.com>
2024-06-09 12:22:44 -0700

    the microservices now have a parent module. this is needed for the next step which is creating a discovery service.

--- inventory-service/src/main/java/com/example/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/example/inventory_service/InventoryServiceApplication.java
@@ -10,23 +10,23 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
 
-	//This will create duplicates everytime the service is started but I would rather have that then set
-	//	spring.jpa.hibernate.ddl-auto=create-drop
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
-		return args->{
-			inventoryRepository.save(Inventory.builder()
-							.skuCode("test1")
-							.quantity(1)
-					.build());
-			inventoryRepository.save(Inventory.builder()
-					.skuCode("test2")
-					.quantity(0)
-					.build());
-		};
-	}
+    //This will create duplicates everytime the service is started but I would rather have that then set
+    //	spring.jpa.hibernate.ddl-auto=create-drop
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            inventoryRepository.save(Inventory.builder()
+                    .skuCode("test1")
+                    .quantity(1)
+                    .build());
+            inventoryRepository.save(Inventory.builder()
+                    .skuCode("test2")
+                    .quantity(0)
+                    .build());
+        };
+    }
 }
